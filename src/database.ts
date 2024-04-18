a13dc9d9f0f1a79f06bc1cf5d9824aba2412a633
FelipeAmorimDev <felipecastroo.ads@gmail.com>
2024-04-18 12:42:00 -0300

    ✅ Configurado para deploy no render

--- src/database.ts
+++ src/database.ts
@@ -1,11 +1,15 @@
+/* eslint-disable prettier/prettier */
 import { knex as setupKnex, Knex } from 'knex'
 import { env } from './env'
 
 const config: Knex.Config = {
   client: 'sqlite3',
-  connection: {
-    filename: env.DATABASE_URL,
-  },
+  connection:
+    env.DATABASE_CLIENT === 'sqlite'
+      ? {
+        filename: env.DATABASE_URL,
+      }
+      : env.DATABASE_URL,
   useNullAsDefault: true,
   migrations: {
     extension: 'ts',
--- src/env/index.ts
+++ src/env/index.ts
@@ -1,18 +1,13 @@
-import { config } from 'dotenv'
+import 'dotenv/config'
 import { z } from 'zod'
-
-if (process.env.NODE_ENV === 'test') {
-  config({ path: '.env.test' })
-} else {
-  config()
-}
 
 const envSchema = z.object({
   NODE_ENV: z
     .enum(['development', 'test', 'production'])
     .default('development'),
+  DATABASE_CLIENT: z.enum(['sqlite', 'pg']),
   DATABASE_URL: z.string(),
-  PORT: z.number().default(3333),
+  PORT: z.coerce.number().default(3333),
 })
 
 const _env = envSchema.safeParse(process.env)
